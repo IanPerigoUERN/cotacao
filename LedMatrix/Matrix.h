dbe1c0e47e15722693bd868f373f24d4835a6554
IanPerigoUERN <ian20240027015@alu.uern.br>
2025-01-29 16:20:52 -0300

    Modularizando a matriz de leds

---
+++ LedMatrix/Matrix.h
@@ -0,0 +1,17 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ws2818b.pio.h"
+
+// Biblioteca gerada pelo arquivo .pio durante compilação.
+#include "ws2818b.pio.h"
+
+// Definição do número de LEDs e pino.
+#define LED_COUNT 25
+#define LED_PIN 7
+
+#endif
